76bc602b73f53f4f885f2644c4b8a3182508d16d
bhashkarjya <bhashkarjyanath1999@gmail.com>
2021-06-28 09:34:47 +0530

    Clean up sock file

--- Server.cs
+++ Server.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.IO;
 
 namespace FDK
 {
@@ -13,7 +14,18 @@ namespace FDK
                     webBuilder.UseStartup<Startup>()
                     .ConfigureKestrel(options =>
                     {
-                        //options.ListenUnixSocket(ctnEnv.FN_LISTENER);
+                        //Console.WriteLine(ctnEnv.FN_LISTENER);
+                        //Console.WriteLine(ctnEnv.SYMBOLIC_LINK);
+                        //Cleaning up the sock file and the soft link to sock file, if it does not get deleted
+                        if(File.Exists(ctnEnv.FN_LISTENER))
+                        {
+                            File.Delete(ctnEnv.FN_LISTENER);
+                        }
+                        if(File.Exists(ctnEnv.SYMBOLIC_LINK))
+                        {
+                            File.Delete(ctnEnv.SYMBOLIC_LINK);
+                        }
+                        options.ListenUnixSocket(ctnEnv.FN_LISTENER);
                         Console.WriteLine("Unix Domain Socket connected");
                     });
                 });
