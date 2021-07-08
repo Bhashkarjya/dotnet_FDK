4773ff5cebaa9e08fee4bad60aae195055075499
Gaurav Saini <gaurav.s.saini@oracle.com>
2021-07-08 22:33:27 +0530

    WIP

--- src/Startup.cs
+++ src/Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System.IO;
 using System;
-using Mono.Unix.Native;
 using System.Threading;
 
 namespace FDK
@@ -39,25 +38,14 @@ namespace FDK
             app.UseMiddleware<ResponseMiddleware>();
             
             applicationLifetime.ApplicationStarted.Register(() => {
-                string UnixFilePath = containerEnvironment.FN_LISTENER;
-                string SoftStorageFileOfTheUnixFilePath = containerEnvironment.SYMBOLIC_LINK;
-                Syscall.symlink(UnixFilePath,SoftStorageFileOfTheUnixFilePath);
-                Syscall.chmod(
-                    SoftStorageFileOfTheUnixFilePath,
-                    FilePermissions.S_IRUSR | FilePermissions.S_IWUSR | FilePermissions.S_IXUSR |
-                    FilePermissions.S_IRGRP | FilePermissions.S_IWGRP | FilePermissions.S_IXGRP |
-                    FilePermissions.S_IROTH | FilePermissions.S_IWOTH | FilePermissions.S_IXOTH
-                );
                 CreateHttpRequest.HttpRequestCreation(containerEnvironment);
-                Thread.Sleep(5);
-
             });
 
             applicationLifetime.ApplicationStopped.Register(() => 
             {
                 Console.WriteLine("Cleaning the sockets before shutting down the application");
-                File.Delete(containerEnvironment.FN_LISTENER);
-                File.Delete(containerEnvironment.SYMBOLIC_LINK);
+ //               File.Delete(containerEnvironment.FN_LISTENER);
+  //              File.Delete(containerEnvironment.SYMBOLIC_LINK);
             });
         }
     }
